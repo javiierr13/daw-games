5d5bb1913ea5268f18988d13dfcc05373e7af30b
javiierr13 <fjavierrj6@gmail.com>
2025-05-04 17:46:55 +0200

    OKdeMomento

---
+++ src/test/java/com/daw_games/DawGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daw_games;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
